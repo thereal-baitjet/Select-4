30fcabc83f72ef705abc99563c1d91c0a83bfa47
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-07-01 19:26:30 -0400

    made ticket a little more astetic

--- select-4/src/components/Wallet.js
+++ select-4/src/components/Wallet.js
@@ -10,43 +10,48 @@ const Wallet = () => {
             marginTop: "5%",
             alignItems: "center",
             width: "60rem",
-            height: "40rem",
+            height: "50rem",
             backgroundImage:
               "url(https://scontent.fewr1-6.fna.fbcdn.net/v/t1.6435-0/p526x296/211686338_346049410255257_3369337830695011129_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_ohc=5JEc2mlmRYEAX9nSiLv&_nc_ht=scontent.fewr1-6.fna&tp=6&oh=996ac2ce8ebb319cb397ddf9dfb82833&oe=60E3A75D)",
           }}
         >
           <Card.Body>
             <Card.Title style={{ fontSize: "99px" }}>SELECT-4</Card.Title>
-            <Card.Text></Card.Text>
+            <Card.Text
+              style={{ borderTop: "5px solid", borderBottom: "5px solid" }}
+            >
+              YOUR CHANCE TO PICK TODAYS WINING NUMBERS
+            </Card.Text>
           </Card.Body>
           <ListGroup className="list-group-flush">
             <ListGroupItem
               style={{
-                border: "none",
+                borderTop: "5px solid",
                 backgroundColor: "#EBCD46",
-                fontSize: "60px",
+                fontSize: "40px",
               }}
             >
               A. 3248
             </ListGroupItem>
             <ListGroupItem
-              style={{ backgroundColor: "#EBCD46", fontSize: "60px" }}
+              style={{ backgroundColor: "#EBCD46", fontSize: "40px" }}
             >
               B. 6789
             </ListGroupItem>
             <ListGroupItem
-              style={{ backgroundColor: "#EBCD46", fontSize: "60px" }}
+              style={{ borderBottom: "5px solid", backgroundColor: "#EBCD46", fontSize: "40px" }}
             >
               C. 4856
             </ListGroupItem>
           </ListGroup>
           <Card.Body>
             <Card.Link
-              style={{ fontFamily: "timesNewRoman", fontSize: "50px" }}
+              style={{ fontFamily: "timesNewRoman", fontSize: "30px", textDecoration: 'none' }}
               href="#keypad"
             >
               PLAY AGAIN
             </Card.Link>
+            <Card.Text style={{ borderTop: "5px solid", borderBottom: "5px solid" }}>GET RICH. GET HAPPY. TODAY.</Card.Text>
           </Card.Body>
         </Card>
       </div>
