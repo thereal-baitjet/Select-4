55a356294c086d10fbe645ac9786f5bb246b3508
Kelly Vaden <klvaden@gmail.com>
2021-07-01 20:20:05 -0400

    Wrote dailyDraw function. Pseudo-coded checkWinners function.

---
+++ select-4/src/utils/checkWinners.js
@@ -0,0 +1,14 @@
+// Author: Kelly
+
+// import winningNumber
+
+function checkWinner(winningNumber) {
+    // GET route to get tickets, from all users, for today's date
+
+    // Loop through array of tickets
+        // Compare ticket to winningNumber
+        // If there is a match, keep track of winning ticket and user
+    
+    // Return array of winners (will usually be empty)
+}
+
