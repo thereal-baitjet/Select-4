08c2f7febbb33e80b401922d3df4fa6a23046709
Kelly Vaden <klvaden@gmail.com>
2021-07-02 21:16:56 -0400

    Made some tweaks to server.js and connection.js for readability. Added schema.sql

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,21 @@
-const Sequelize=require("sequelize")
-require("dotenv").config()
+const Sequelize  = require("sequelize");
+require("dotenv").config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-    } else {
+    sequelize = new Sequelize(process.env.JAWSDB_URL); // If hosted on Heroku, connect to Sequelize through Heroku
+} else { // If working locally, connect to Sequelize through credentials in ENV file
     sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-    }
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
     );
-    }
-
-    // sequelize.sync({force:false}).then(()=>{
-    //     console.log("connected")   
-    //  })
+}
     
-    module.exports = sequelize
+module.exports = sequelize
