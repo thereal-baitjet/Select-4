431089a5ece0bb665f00c61dada93c22025e9fff
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-07-13 21:04:45 -0400

    added logo to draw looked too empty

--- select-4/src/components/Drawing/Drawing.js
+++ select-4/src/components/Drawing/Drawing.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Style.css";
+import Header3 from "../../images/Header3.png";
 
 
 
 const Drawing = () => {
   return (
     <div>
-      <h1 style={{marginTop: '20px',  color: 'white'}}>DRAWING, if your numbers match these you are the $1000.00 Winner</h1>
+      <img src={Header3} style={{ maxWidth: '50%', height: 'auto' }}alt="Logo"/>
+      <h1 style={{marginTop: '0px',  color: 'white'}}>DRAWING, if your numbers match these you are the $1000.00 Winner</h1>
     <div class="wrap">
   <section class="stage">
         <figure class="ball">
