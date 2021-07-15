fd6d781573930f3099af9fac290323efaf14b03c
John-Mancini <mancinijohn5988@gmail.com>
2021-07-14 21:23:27 -0400

    fixed classnames

--- select-4/src/components/Home.js
+++ select-4/src/components/Home.js
@@ -4,16 +4,21 @@ import Header3 from "../images/Header3.png";
 const Home = () => {
   return (
     <div>
-      <img src={Header3} style={{ maxWidth: '100%', height: 'auto' }}alt="Logo"/>;
+      <img
+        src={Header3}
+        style={{ maxWidth: "100%", height: "auto" }}
+        alt="Logo"
+      />
+      ;
       <a
         href="#keypad"
-        class="btn btn-warning btn-lg btn-block"
+        className="btn btn-warning btn-lg btn-block"
         role="button"
         aria-pressed="true"
       >
         Start Game
       </a>
-      <p style={{color: "white"}}>
+      <p style={{ color: "white" }}>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
