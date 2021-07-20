21e69a3170f37ea90bac6d19b6c05f697ea21a79
John-Mancini <mancinijohn5988@gmail.com>
2021-07-20 16:45:08 -0400

    fixed classNames

--- controllers/api/ticketRoutes.js
+++ controllers/api/ticketRoutes.js
@@ -1,15 +1,19 @@
-const router = require('express').Router();
-const { Ticket } = require('../../models');
+const router = require("express").Router();
+const { Ticket } = require("../../models");
 
 // Route: POST /api/tickets/
 // Allows user to create a new ticket
-router.post('/', async (req, res) => {
-    try {
-        const ticketData = await Ticket.create(req.body);
-        res.status(200).json(ticketData);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.post("/", async (req, res) => {
+  console.log("post.tickets");
+  console.log(req.body);
+  try {
+    const ticketData = await Ticket.create(req.body);
+
+    console.log(ticketData);
+    res.status(200).json(ticketData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // Route: GET /api/tickets/
@@ -17,13 +21,31 @@ router.post('/', async (req, res) => {
 // TODO: Connect to front-end Wallet component
 // TODO: Need to consider logged-in user once Auth is set up
 // TODO: Should this be designed to only pull tix for the current date?
-router.get('/', async (req, res) => {
-    try {
-        const ticketData = await Ticket.findAll();
-        res.status(200).json(ticketData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const ticketData = await Ticket.findAll();
+    res.status(200).json(ticketData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/dummyTicket", async (req, res) => {
+  try {
+    const ticketData = await Ticket.create({
+      user_id: 5,
+      number: "5555",
+      date: "2021-07-15T01:38:49.151Z",
+    });
+
+    console.log(ticketData);
+    res.status(200).json(ticketData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get("/test", async (req, res) => {
+  res.send("test");
 });
 
 module.exports = router;
--- select-4/src/components/Keypad.js
+++ select-4/src/components/Keypad.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import API from '../utils/API'
-import "./keypad.css"
+import API from "../utils/API";
+import "./keypad.css";
 
 const Keypad = (props) => {
   const [clickedNumber, setClickedNumber] = useState("");
@@ -11,81 +11,167 @@ const Keypad = (props) => {
     let newNumber = clickedNumber + number;
     if (newNumber.length <= 4) {
       setClickedNumber(newNumber);
-    //   props.pickNumber(newNumber);
-    //   setClickedNumber("");
+      //   props.pickNumber(newNumber);
+      //   setClickedNumber("");
     }
     // else {  }
   };
 
   const clear = () => {
-    setClickedNumber('');
+    setClickedNumber("");
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // use POST route to send the ticket to the database using logged in user and today's date
     // Only call API route if ticket is exactly 4 digits
-      // TODO: Need to make an error message if not
+    // TODO: Need to make an error message if not
     if (clickedNumber.length === 4) {
       API.createTicket({
-        "number": clickedNumber,
-        "date": new Date(),
-        "user_id": 5 // req.session.user_id // TODO: Once Auth is finished, user_id needs to be equal to logged in user
-      })
-      .catch(err => console.log(err));
-      setClickedNumber('');
+        number: clickedNumber,
+        date: new Date(),
+        user_id: 5, // req.session.user_id // TODO: Once Auth is finished, user_id needs to be equal to logged in user
+      }).catch((err) => console.log(err));
+      setClickedNumber("");
     }
-  }
+  };
 
   return (
     <div className="backGround">
-      <h1 style={{color:"red", fontStyle:"revert", }}>Choose your 4 numbers</h1>
+      <h1 style={{ color: "red", fontStyle: "revert" }}>
+        Choose your 4 numbers
+      </h1>
       <div className="container py-4">
-        <div className="body" style={{
-          backgroundImage: 'url("Keypadbackground.jpg")', 
-          backgroundRepeat: 'no-repeat', 
-          backgroundPosition: 'center', 
-          backgroundSize: '1500px 1500px'}}>
-            <div className="container py-4" style={{color: 'white', fontSize: '100px'}}>
-            <h1>Input four numbers per line, max 3 lines per game for each drawing, refer to wallet page for your selected ticket</h1>
+        <div
+          className="body"
+          style={{
+            backgroundImage: 'url("Keypadbackground.jpg")',
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "1500px 1500px",
+          }}
+        >
+          <div
+            className="container py-4"
+            style={{ color: "white", fontSize: "100px" }}
+          >
+            <h1>
+              Input four numbers per line, max 3 lines per game for each
+              drawing, refer to wallet page for your selected ticket
+            </h1>
 
             {/* Here is where typed numbers appear and get sent to database */}
-            <form onSubmit={ handleSubmit }>
-              <input type='text' name='ticket' value={ clickedNumber }/>
+            <form onSubmit={handleSubmit}>
+              <input disabled type="text" name="ticket" value={clickedNumber} />
             </form>
 
             <br></br>
-              <div className="row">
-                <div className="col-auto mx-auto bg-white rounded shadow">
-                  <div className="btn-group-vertical mx-4 my-5" role="group" aria-label="Basic example">
-                    <div className="btn-group btn-group-lg">
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary border-bottom-0 rounded-0">1</button>
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary border-bottom-0">2</button>
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary border-bottom-0 rounded-0">3</button>
-                    </div>
-                    <div className="btn-group btn-group-lg">
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary border-bottom-0 rounded-0">4</button>
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary border-bottom-0">5</button>
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary border-bottom-0 rounded-0">6</button>
-                    </div>
-                    <div className="btn-group btn-group-lg">
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary rounded-0">7</button>
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary">8</button>
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary rounded-0">9</button>
-                    </div>
-                    <div className="btn-group btn-group-lg">
-                      <button type="button" onClick={clear} className="btn btn-outline-secondary">X</button> 
-                      <button type="button" onClick={clickButton} className="btn btn-outline-secondary">0</button> 
-                      <button type="button" onClick={handleSubmit} className="btn btn-outline-secondary">{'>>'}</button> 
-                    </div>
+            <div className="row">
+              <div className="col-auto mx-auto bg-white rounded shadow">
+                <div
+                  className="btn-group-vertical mx-4 my-5"
+                  role="group"
+                  aria-label="Basic example"
+                >
+                  <div className="btn-group btn-group-lg">
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary border-bottom-0 rounded-0"
+                    >
+                      1
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary border-bottom-0"
+                    >
+                      2
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary border-bottom-0 rounded-0"
+                    >
+                      3
+                    </button>
+                  </div>
+                  <div className="btn-group btn-group-lg">
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary border-bottom-0 rounded-0"
+                    >
+                      4
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary border-bottom-0"
+                    >
+                      5
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary border-bottom-0 rounded-0"
+                    >
+                      6
+                    </button>
+                  </div>
+                  <div className="btn-group btn-group-lg">
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary rounded-0"
+                    >
+                      7
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary"
+                    >
+                      8
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary rounded-0"
+                    >
+                      9
+                    </button>
+                  </div>
+                  <div className="btn-group btn-group-lg">
+                    <button
+                      type="button"
+                      onClick={clear}
+                      className="btn btn-outline-secondary"
+                    >
+                      X
+                    </button>
+                    <button
+                      type="button"
+                      onClick={clickButton}
+                      className="btn btn-outline-secondary"
+                    >
+                      0
+                    </button>
+                    <button
+                      type="button"
+                      onClick={handleSubmit}
+                      className="btn btn-outline-secondary"
+                    >
+                      {">>"}
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
