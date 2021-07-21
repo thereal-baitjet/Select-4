e80eb6086130bfc4846e47f2ce18f2d4a4bd718a
Christine Osmak <christineosmak@gmail.com>
2021-07-20 20:32:55 -0400

    fixed start button spot

--- select-4/src/components/Home.js
+++ select-4/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         style={{ maxWidth: "100%", height: "auto" }}
         alt="Logo"
       />
-      ;
+      <br></br>
       <a
         href="#keypad"
         className="btn btn-warning btn-lg btn-block"
@@ -18,8 +18,11 @@ const Home = () => {
       >
         Start Game
       </a>
+      <br></br>
+      <br></br>
       <p style={{ color: "white" }}>
-        Why play Select-4? Lotteries can be dated back 1000's of years. Specifically throughout history, places around the world like France, England, 
+        Why play Select-4? <br>
+        </br><br></br>Lotteries can be dated back 1000's of years. Specifically throughout history, places around the world like France, England, 
        the early United States, Spain, China and many more have enjoyed this classicaly historic game. As the world changed throughout the years, so has the game. 
        Our modern day version of the lottery has adapted to our culture and especially now, our "new normal". We want to give our customers the traditional enjoyment
        we've all grown to love, but in a safe and convenient enviroment..through their own phone. Let's stay safe, save time, make money, and especially have fun playing Select-4.
