88066eea1ba0aa280d8df48194d2000d2f8a5fe3
Kelly Vaden <klvaden@gmail.com>
2021-07-22 19:34:03 -0400

    Changed controllers/index to allow deploy

--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,8 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//     res.sendFile(path.join(__dirname, '../select-4/build/index.html'));
-// });
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '../select-4/build/index.html'));
+});
 
 module.exports = router;
